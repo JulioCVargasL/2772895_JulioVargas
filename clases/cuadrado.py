58c7b330804c8588284b48ada5b1553e416e8b57
JulioVargas <2.0.jcvl92@gmail.com>
2023-11-25 18:26:27 -0500

    Commit de clases

---
+++ clases/cuadrado.py
@@ -0,0 +1,9 @@
+#metodo area y metodo perimetro
+
+class cuadrado:
+	def area(self):
+		print("Hola soy area")
+	def perimetro(self):
+		print("Hola soy perimetro")
+
+	
---
+++ clases/instancias.py
@@ -0,0 +1,6 @@
+from cuadrado import cuadrado
+#from cuadrado import perimetro
+cuadrado1 = cuadrado()
+cuadrado1.perimetro()
+cuadrado1.area()
+
