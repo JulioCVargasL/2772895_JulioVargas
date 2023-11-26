283aa9b01bc0a7af60bbf7f183bc85ed3059010f
JulioVargas <2.0.jcvl92@gmail.com>
2023-11-25 20:09:54 -0500

    ejemplo de encapsulamiento

--- clases/cuadrado.py
+++ clases/cuadrado.py
@@ -1,9 +1,19 @@
 #metodo area y metodo perimetro
 
 class cuadrado:
+	
+	#se crea el constructor, se inicializa el valor del lado =0 para que Python pueda iniciar el procedimiento
+	def __init__(self, lado = 0):
+		self._lado = lado
+	# se crea un metodo que nos permita traer el el atributo privado _lado
+	def set_lado(self, lado):
+		self._lado = lado
+	# Se crea el metodo del area
 	def area(self):
-		print("Hola soy area")
+		print(f"Hola soy el area:	{self._lado**2} u^2")
+	# Se crea el metodo del perimetro entre {} estan las operaciones respectivas
 	def perimetro(self):
-		print("Hola soy perimetro")
+		print(f"Hola soy el perimetro:	{4*self._lado} u")
+
 
 	
--- clases/instancias.py
+++ clases/instancias.py
@@ -1,6 +1,10 @@
 from cuadrado import cuadrado
 #from cuadrado import perimetro
+
 cuadrado1 = cuadrado()
+
+nuevo_lado = int(input("\nIngresa el lado: "))
+cuadrado1.set_lado(nuevo_lado)
 cuadrado1.perimetro()
 cuadrado1.area()
 
